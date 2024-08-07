d62dfdb5a685e80122adf3a182b7bb1fc3cbcae1
Stevie Kelvin <steviekelvinsilvabarbosa4@gmail.com>
2024-08-07 12:35:42 -0300

    Update users_routes.php

--- routes/users/users_routes.php
+++ routes/users/users_routes.php
@@ -3,5 +3,9 @@
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
+/**
+ * Rotas de autenticação e usuario
+ */
+
 Route::middleware('auth:api')->get('/', [UserController::class, 'show']);
 Route::post('/login', [UserController::class, 'login']);
